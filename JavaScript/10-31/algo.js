5365ca7cf3192895a0df18c9d12c83f9f0744057
Kennedy Vandel <34487378+vandelk@users.noreply.github.com>
2023-10-31 08:33:10 -0700

    diagonal difference algo

---
+++ JavaScript/10-31/algo.js
@@ -0,0 +1,51 @@
+/* 
+  Given a square matrix (2d array) of integers
+  Calculate the absolute difference between the sums of its diagonals
+    - top left to bottom right diagonal
+    - top right to bottom left diagonal
+*/
+const squareMatrix1 = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [9, 8, 9]
+  ];
+  const expected1 = 2;
+  /* 
+    left to right diagonal: 1 + 5 + 9 = 15
+    right to left diagonal: 3 + 5 + 9 = 17
+    absolute difference = 2
+  */
+  
+  const squareMatrix2 = [
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+    [1, 2, 3, 4, 5],
+  ];
+  const expected2 = 0;
+  /* 
+    left to right diagonal: 1 + 2 + 3 + 4 + 5 = 15
+    right to left diagonal: 5 + 4 + 3 + 2 + 1 = 15
+    absolute difference = 0
+  */
+  
+  
+  function diagonalDifference(sqrMatrix) {
+    // R.I.O.T.
+    // think about the steps first
+    // DRIVER 🚗
+    // PRESENTER 👨‍🏫
+    // NAVIGATOR 🧭
+    let sumLToR = 0;
+    let sumRToL = 0;
+
+    for(let i = 0; i < sqrMatrix.length; i++) {
+        sumLToR += sqrMatrix[i][i];
+        sumRToL += sqrMatrix[i][sqrMatrix.length-i-1];
+    }
+    return Math.abs(sumLToR-sumRToL);
+  }
+
+  console.log(diagonalDifference(squareMatrix1));
+  console.log(diagonalDifference(squareMatrix2));
